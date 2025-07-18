ff14e9433eccb0da2759192f0e9da16775c3f31f
mostafaelhawary1987 <hoohoo1987@gmail.com>
2025-07-18 12:29:32 +0300

    index2

--- js/main.js
+++ js/main.js
@@ -15,100 +15,72 @@ $(document).ready(function () {
     $('.overlay').fadeOut();
     $('body').removeClass('ovh');
   });
-
-
-  $('.slider-head').owlCarousel({
-  autoplay: false,
-  rtl: document.dir == 'rtl' ? true : false,
-  loop:false,
-  dots:false,
-  nav: true,
-  items: 1,
-  navText: ["<span class='fa fa-chevron-right'></span>","<span class='fa fa-chevron-left'></span>"]
-  
-});
-  var a = 0;
-$(window).scroll(function () {
-  if ($("div").hasClass("numbers-flex")) {
-    if (
-      a === 0 &&
-      $(this).scrollTop() >= $(".numbers-flex").offset().top - 500
-    ) {
-      $(".number span").each(function () {
-        $(this)
-          .prop("Counter", 0)
-          .animate(
-            {
-              Counter: $(this).text(),
-            },
-            {
-              duration: 1000,
-              easing: "swing",
-              step: function (now) {
-                $(this).text(Math.ceil(now));
-              },
-            }
-          );
-      });
-      a = 1;
+  $('.partners_slider').owlCarousel({
+  items:4,
+  autoplay:true,
+  rtl:true,
+  loop: true,
+  margin:18,
+  nav:false,
+     dots:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    425:{
+      items:1
+    },
+    768:{
+      items:2
+    },
+    991:{
+      items:3
+    },
+    1199:{
+      items:4
+    },
+    1200:{
+      items:4
     }
   }
 });
-        var owl = $("#banner-slider-demo-1").owlCarousel({
-        items: 1,
-        
-        autoplay: false,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true, 
-        dots: true,
-        dotsContainer: '#carousel-custom-dots',
-        nav: true,
-        navRewind: true,
-         navText: ["<span class='fa-regular fa-circle-arrow-right'></span>","<span class='fa-regular fa-circle-arrow-left'></span>"],
-        loop: false, });
-        $('.owl-dot').click(function(){owl.trigger('to.owl.carousel', [$(this).index(), 300]);});
-        //Random index generator
-        function randomPosition(){
-        var r_hb = $('#carousel-custom-dots li').length;
-        var randomize = null;
-        randomize=1
-        
-        
-        if (randomize != 0) {
-        return (Math.floor(Math.random()* r_hb));
-        }
-        else { return 0 ;}
-        }
-        //Sort random function
-        function random(owlSelector){
-        owlSelector.children().sort(function(){
-        return Math.round(Math.random()) - 0.5;
-        }).each(function(){
-        $(this).appendTo(owlSelector);
-        });
-        }
-        ///////// landing header /////////
-    $('a[data-scroll^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+// var $mediaElements = $('.pack');
+// $('.tab-ck').click(function (e) {
+//     e.preventDefault();
+//     var filterVal = $(this).data('filter');
+//     $(".tab-ck").not(this).removeClass("active");
+//     $(this).toggleClass("active");
+//     $mediaElements.removeClass("active").filter('.' + filterVal).addClass("active");
 
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
+// });
 
-        var target = $(this).attr("data-scroll"),
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 100
-        }, 500, 'swing');
-        if ($(window).width() < 992) {
-            $("nav").toggleClass("active");
-            $("body").toggleClass("overflow");
-            $(".menu-overlay").fadeToggle(400);
-        }
-    });
+
+var $mediaElements = $('.course');
+$('.tabs-sec .tab-a').click(function (e) {
+    e.preventDefault();
+    var filterVal = $(this).data('filter');
+    $(".tabs-sec .tab-a").not(this).removeClass("active");
+    $(this).toggleClass("active");
+    $mediaElements.removeClass("active").filter('.' + filterVal).addClass("active");
+    // if (filterVal === 'one') {
+    //   $mediaElements.show();
+    // } else {
+        
+    // }
+});
+
+
+//   $('.slider-head').owlCarousel({
+//   autoplay: false,
+//   rtl: document.dir == 'rtl' ? true : false,
+//   loop:false,
+//   dots:false,
+//   nav: true,
+//   items: 1,
+//   navText: ["<span class='fa fa-chevron-right'></span>","<span class='fa fa-chevron-left'></span>"]
+  
+// });
+ 
     
 
 });
